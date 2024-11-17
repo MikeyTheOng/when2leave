69e2dcd6e01d85594809c203b9ee5e3bcf9dbac6
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-11-17 13:28:41 +0800

    chore: Comment out redirection for testing

--- app/catch-the-bus/page.tsx
+++ app/catch-the-bus/page.tsx
@@ -2,13 +2,13 @@
 import NotificationTimer from "@/components/notificationTimer";
 
 import { useTransitConfig } from "@/hooks/useTransitConfig";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export default function CatchTheBus() {
     const transitConfig = useTransitConfig();
     console.log("transitConfig:", transitConfig); // ! Debugging
 
-    if (!transitConfig) redirect('/transit-config');
+    // if (!transitConfig) redirect('/transit-config');
     return <div>
         <h1>Catch The Bus</h1>
         <NotificationTimer />
