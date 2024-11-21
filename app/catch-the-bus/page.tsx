bb71dedf1b360e141e49e9929cf179c4b81f360b
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-11-22 00:45:41 +0800

    feat: Update catch-the-bus page to redirect to transit-config if transitConfig not found in localStorage

--- app/catch-the-bus/page.tsx
+++ app/catch-the-bus/page.tsx
@@ -1,13 +1,33 @@
 "use client";
 import { useTransitConfig } from "@/hooks/useTransitConfig";
-// import { redirect } from "next/navigation";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/shadcn/button";
+import { H1 } from "@/components/shared/headers";
 
 export default function CatchTheBus() {
-    const transitConfig = useTransitConfig();
-    console.log("transitConfig:", transitConfig); // ! Debugging
+    const router = useRouter();
+    const { transitConfig, isLoading } = useTransitConfig();
 
-    // if (!transitConfig) redirect('/transit-config');
+    useEffect(() => {
+        if (!isLoading && transitConfig === null) {
+            console.log("transitConfig:", transitConfig);
+            router.push('/transit-config');
+        }
+    }, [transitConfig, isLoading, router]);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return <div>
-        <h1 className="text-center">Catch The Bus</h1>
+        <H1 className="text-center">Catch The Bus</H1>
+        <Button className="w-full">I&apos;m Ready to Leave!</Button>
+        <div className="overflow-x-auto">
+            <pre className="whitespace-pre">
+                Transit Config: {JSON.stringify(transitConfig, null, 2)}
+            </pre>
+        </div>
     </div>;
 }
--- hooks/useTransitConfig.ts
+++ hooks/useTransitConfig.ts
@@ -3,13 +3,15 @@ import { TransitConfig } from '@/lib/types';
 
 export const useTransitConfig = () => {
     const [transitConfig, setTransitConfig] = useState<TransitConfig | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const stored = localStorage.getItem('transitConfig');
         if (stored) {
             setTransitConfig(JSON.parse(stored));
         }
+        setIsLoading(false);
     }, []);
 
-    return transitConfig;
+    return { transitConfig, isLoading };
 };
