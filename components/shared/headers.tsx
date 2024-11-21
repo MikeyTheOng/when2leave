e876fd4c643fab3f21f65f0bf7161f618a43e021
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-11-22 00:56:19 +0800

    refactor: Add className prop to H1 component

--- components/shared/headers.tsx
+++ components/shared/headers.tsx
@@ -1,5 +1,5 @@
-const H1 = ({ children }: { children: React.ReactNode }) => {
-    return <h1 className="text-h3 sm:text-h1 font-bold">{children}</h1>
+const H1 = ({ className, children }: { className?: string, children: React.ReactNode }) => {
+    return <h1 className={`text-h3 sm:text-h1 font-bold ${className}`}>{children}</h1>
 }
 
 export { H1 };
